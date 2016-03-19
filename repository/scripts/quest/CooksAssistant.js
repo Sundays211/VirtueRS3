2b58feef6f43ff704cb00da3f9e257f05d5111af
jarryd229 <jarrydbrighton@hotmail.com>
2016-03-19 23:38:12 +1100

    Update CooksAssistant.js

--- repository/scripts/quest/CooksAssistant.js
+++ repository/scripts/quest/CooksAssistant.js
@@ -22,9 +22,24 @@
 
 var CooksAssistant = {
 		showQuestJournal : function (player, questLog) {
-			questLog.setJournalLine(player, 1, "Line 1 for Cook's Assistant");
-			questLog.setJournalLine(player, 2, "Line 2 for Cook's Assistant");
-			questLog.setJournalLine(player, 3, "Line 3 for Cook's Assistant");
-			questLog.setJournalLine(player, 4, "...");
+	     if(questApi.isFinished(player, 6)) {
+		questLog.setJournalLine(player, 1, "");
+		questLog.setJournalLine(player, 2, "<col=999999>It was the Duke of Lumbridge's birthday, but his cook had forgotten to buy the");
+		questLog.setJournalLine(player, 3, "<col=999999>ingredients he needed to bake a birthdat cake. I brought the cook an egg, a pot");
+		questLog.setJournalLine(player, 4, "<col=999999>of flour and a bucket of milk and the cook made a delicious-looking cake with");
+		questLog.setJournalLine(player, 5, "<col=999999>them");
+		questLog.setJournalLine(player, 6, "");
+		questLog.setJournalLine(player, 7, "<col=FFFFFF>QUEST COMPLETE!");
+		questLog.setJournalLine(player, 8, "<col=EB981F>I gained <col=EBE076>1 Quest Point<col=EB981F>, <col=EBE076>20 sardines<col=EB981F>, <col=EBE076>500 coins <col=EB981F>and <col=EBE076> 300 Cooking XP<col=EB981F>.");
+		questLog.setJournalLine(player, 9, "<col=EB981F>I have also gained <col=EBE076>two prize keys<col=EB981F> for <col=EBE076>Treasure Hunter<col=EB981F>!");
+	        questLog.setJournalLine(player, 10, "<col=EB981F>The cook now also lets me use his high-quality <col=EBE076>range<col=EB981F>, which burns certain low-");
+		questLog.setJournalLine(player, 11, "<col=EB981F>level dishes less often thsn other ranges.");
+	} else {
+		questLog.setJournalLine(player, 1, "");
+		questLog.setJournalLine(player, 2, "<col=EB981F>I can start this quest by speaking to the <col=EBE076>Cook<col=EB981F> in the <col=EBE076>kitchen<col=EB981F> of <col=EBE076>Lumbridge");
+		questLog.setJournalLine(player, 3, "<col=EBE076>Castle<col=EB981F>.");
+
+
+	}
 		}
 }
