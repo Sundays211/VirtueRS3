c93a1f69963bb966622534dfd4cdc3670244ff32
Sundays211 <sundays211@gmail.com>
2017-05-02 08:52:37 +0000

    Added message for players who don't have the right items to access a runecrafting altar

--- repository/scripts/skill/runecrafting/mysterious-runes.js
+++ repository/scripts/skill/runecrafting/mysterious-runes.js
@@ -88,7 +88,7 @@ module.exports = function(scriptManager) {
 				return;
 			}
 		} else if (!inv.isWearing(player, runes.tiara) && !inv.isWearing(player, runes.staff)) {
-			util.defaultHandler(ctx, "mysterious runes");
+			ENGINE.sendMessage(player, runes.noAccessMsg);
 			return;
 		}
 		ENGINE.sendMessage(player, "You feel a powerful force take hold of you.");		
@@ -105,73 +105,85 @@ module.exports = function(scriptManager) {
 			destination : coords(2841, 4829, 0),
 			talisman : 1438,
 			tiara : 5527,
-			staff : 13630
+			staff : 13630,
+			noAccessMsg : "You need an air talisman to access the Air Altar."
 		},
 		MIND : {
 			destination : coords(2792, 4827, 0),
 			talisman : 1448,
 			tiara : 5529,
-			staff : 13631
+			staff : 13631,
+			noAccessMsg : "You need a mind talisman to access the Mind Altar."
 		},
 		WATER : {
 			destination : coords(3482, 4838, 0),
 			talisman : 1444,
 			tiara : 5531,
-			staff : 13632
+			staff : 13632,
+			noAccessMsg : "You need a water talisman to access the Water Altar."
 		},
 		EARTH : {
 			destination : coords(2655, 4830, 0),
 			talisman : 1440,
 			tiara : 5535,
-			staff : 13633
+			staff : 13633,
+			noAccessMsg : "You need an earth talisman to access the Earth Altar."
 		},
 		FIRE : {
 			destination : coords(2574, 4848, 0),
 			talisman : 1442,
 			tiara : 5537,
-			staff : 13634
+			staff : 13634,
+			noAccessMsg : "You need a fire talisman to access the Fire Altar."
 		},
 		BODY : {
 			destination : coords(2522, 4835, 0),
 			talisman : 1446,
 			tiara : 5533,
-			staff : 13635
+			staff : 13635,
+			noAccessMsg : "You need a body talisman to access the Body Altar."
 		},
 		COSMIC : {
 			destination : coords(2142, 4853, 0),
 			talisman : 1454,
 			tiara : 5539,
-			staff : 13636
+			staff : 13636,
+			noAccessMsg : "You need a cosmic talisman to access the Cosmic Altar."
 		},
 		CHAOS : {
 			destination : coords(2281, 4837, 0),
 			talisman : 1452,
 			tiara : 5543,
-			staff : 13637
+			staff : 13637,
+			noAccessMsg : "You need a chaos talisman to access the Chaos Altar."
 		},
 		NATURE : {
 			destination : coords(2398, 4841, 0),
 			talisman : 1462,
 			tiara : 5541,
-			staff : 13638
+			staff : 13638,
+			noAccessMsg : "You need a nature talisman to access the Nature Altar."
 		},
 		LAW : {
             destination : coords(2464, 4818, 0),
             talisman : 1458,
             tiara : 5545,
-            staff : 13639
+            staff : 13639,
+			noAccessMsg : "You need a law talisman to access the Law Altar."
 		},
 		DEATH : {
 		    destination : coords(2208, 4830, 0),
 		    talisman : 1456,
 		    tiara : 5547,
-		    staff : 13640
+		    staff : 13640,
+			noAccessMsg : "You need a death talisman to access the Death Altar."
 		},
 		BLOOD : {
 		    destination : coords(2468, 4889, 1),
 		    talisman : 1450,
 		    tiara : 5549,
-		    staff : 13641
+		    staff : 13641,
+			noAccessMsg : "You need a blood talisman to access the Blood Altar."
 		}
 	};
 };
