8af9ec87acfd6c28aabf386ea4f2c381ca2e4e70
Sundays211 <sundays211@gmail.com>
2017-06-10 06:19:10 +0000

    Fixed jshint warnings & import errors in widget/frame module

--- repository/scripts/widget/settings/combat-settings.js
+++ repository/scripts/widget/settings/combat-settings.js
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+/* globals EventType */
 var varbit = require('../../core/var/bit');
 var util = require('../../core/util');
 var chat = require('../../chat');
@@ -31,27 +31,30 @@ module.exports = (function () {
 	
 	function init (scriptManager) {
 		scriptManager.bind(EventType.IF_BUTTON, 1503, function (ctx) {
+			var player = ctx.player;
 			switch (ctx.component) {
             case 2://sheathe
-			player.switchSheathing();
-			return;	
+				player.switchSheathing();
+				return;	
 			case 4://special attack
-			player.getCombatSchedule().updateAdrenaline(1000);
-		    player.getCombatSchedule().setSpecialEnabled(!player.getCombatSchedule().isSpecialEnabled());
-			player.getCombatSchedule().setDefaultAttack();
-			chat.sendMessage(ctx.player, "Special is " + (player.getCombatSchedule().isSpecialEnabled() ? "enabled." : "disabled."));
-			return true;
+				player.getCombatSchedule().updateAdrenaline(1000);
+			    player.getCombatSchedule().setSpecialEnabled(!player.getCombatSchedule().isSpecialEnabled());
+				player.getCombatSchedule().setDefaultAttack();
+				chat.sendMessage(ctx.player, "Special is " + (player.getCombatSchedule().isSpecialEnabled() ? "enabled." : "disabled."));
+				return;
 		    case 49://retaliate
-			var wasRetaliating = varbit(ctx.player, 462) == 0;
-			varbit(ctx.player, 462, wasRetaliating ? 1 : 0);
-			return;
+				var wasRetaliating = varbit(ctx.player, 462) === 0;
+				varbit(ctx.player, 462, wasRetaliating ? 1 : 0);
+				return;
 		    case 32://attack
 		    case 36://balance
 		    case 40://strength
 		    case 44://defence
+		    	util.defaultHandler(ctx, "Combat Settings");
+				return;
 			default:
-			util.defaultHandler(ctx, "Combat Settings");
-			return;
+				util.defaultHandler(ctx, "Combat Settings");
+				return;
 			}
 		});
 		
