11e2051c848f88c7dc5e58e612ddf166b350d5cb
jarryd229 <jarrydbrighton@hotmail.com>
2018-03-25 13:53:45 +1100

    fixed world map "you are here"
    fixed world map "you are here"

--- repository/scripts/skill/mining/basic.js
+++ repository/scripts/skill/mining/basic.js
@@ -162,21 +162,26 @@ module.exports = (function () {
 	};
 	
 	function init (scriptManager) {
+		
+		scriptManager.bind(EventType.OPLOC1, [11556,11557,72078, 72079, 72080,72084,72085,72086,72089,72090,72091], function (ctx) {
+            chat.sendMessage(ctx.player, "There is no ore currently available in this rock.");
+		});
+		
 		scriptManager.bind(EventType.OPLOC1, [ 2108, 2109, 5766, 5767, 9711, 9713, 10577, 10578, 
-			10579, 10949, 11189, 11190, 11191, 14904, 14905, 15503, 15504, 15505, 32429, 32430, 32431, 
-	        72075, 72076, 72077, 72078, 72079, 72080, 75619, 75620 ], function (ctx) {
+			10579, 10949, 11189, 11190, 11191, 14904, 14905, 15503, 15504, 15505, 32429, 32430, 32431, 67006, 67007, 67008,
+	        72075, 72076, 72077, 75619, 75620 ], function (ctx) {
 				mineRock(ctx.player, ctx.location, RockType.CLAY);
 		});
 		
 		scriptManager.bind(EventType.OPLOC1, [2090, 2091, 3027, 3229, 5779, 5780, 5781, 9708, 9709, 
 			9710, 11936, 11937, 11938, 11960, 11961, 11962, 14906, 14907, 18991, 18992, 18993, 21284, 21285, 
-	        21286, 29230, 29231, 72098, 72099, 72100, 75632, 75633, 75634, 88640, 88876, 88878], function (ctx) {
+	        21286, 29230, 29231, 67012, 67013, 67014, 72098, 72099, 72100, 75632, 75633, 75634, 88640, 88876, 88878], function (ctx) {
 				mineRock(ctx.player, ctx.location, RockType.COPPER);
 		});
 		
 		scriptManager.bind(EventType.OPLOC1, [ 2094, 2095, 3038, 3245, 5776, 5777, 5778, 9714, 9716, 11933, 
 			11934, 11935, 11957, 11958, 11959, 14902, 14903, 18994, 18995, 18996, 19024, 19025, 19026, 21293, 21294, 
-	        21295, 29227, 29229, 72092, 72093, 72094, 75629, 75630, 75631, 88642, 88877, 88879 ], function (ctx) {
+	        21295, 29227, 29229, 67009, 67010, 67011, 72092, 72093, 72094, 75629, 75630, 75631, 88642, 88877, 88879 ], function (ctx) {
 				mineRock(ctx.player, ctx.location, RockType.TIN);
 		});
 		
@@ -186,7 +191,7 @@ module.exports = (function () {
 		
 		scriptManager.bind(EventType.OPLOC1, [ 2092, 2093, 5773, 5774, 5775, 6943, 6944, 9717, 9718, 9719, 
 			11954, 11955, 11956, 14856, 14857, 14858, 14913, 14914, 19000, 19001, 19002, 21281, 21282, 
-			21283, 29221, 29222, 29223, 32441, 32442, 32443, 32451, 32452, 37307, 37308, 37309, 72081, 72082, 
+			21283, 29221, 29222, 29223, 32441, 32442, 32443, 32451, 32452, 37307, 37308, 37309, 67004, 67005, 72081, 72082, 
 			72083, 75616, 75617, 75626, 75627, 75628, 88641 ], function (ctx) {
 				mineRock(ctx.player, ctx.location, RockType.IRON);
 		});
@@ -278,99 +283,58 @@ module.exports = (function () {
 	function getEmptyId (player, locTypeId) {
 		switch (locTypeId) {
 			
-		    case 3027:
-			case 3032:
-		    case 3038:
-			case 3040:
-			case 3041:
+			//case 2096://coal
+			
+		    case 3027://copper
+			case 3032://coal
+		    case 3038://tin
+			case 3040://adamantite
+			case 3041://mithril
 		        return 3227;
 				
-			case 3229:
-			case 3233:
-			case 3245:
-			case 3273:
-			case 3280:
+			case 3229://copper
+			case 3233://coal
+			case 3245://tin
+			case 3273://adamantite
+			case 3280://mithril
 		        return 3281;	
             ///////////////limestone//////////////
-			case 4027:
+			case 4027://limestone
 				return 4028;
 				
-			case 4028:
+			case 4028://limestone
 				return 4029;
 			
-            case 4029:
+            case 4029://limestone
 				return 4030;			
 			//////////////////////////////////
-			case 5782:
-			case 5773:	
-			case 5770:
-			case 5776:
-			case 5779:
+			
+			case 5766://clay
+			case 5768://gold
+			case 5770://coal
+			case 5773://iron
+			case 5776://tin
+			case 5779://copper
+			case 5782://adamantite
+			case 5784://mithril
 			    return 5763;				
-				
-			case 5771:
-            case 5774:			
-			case 5777:
-			case 5780:
+			
+			case 5767://clay
+			case 5769://gold
+			case 5771://coal
+            case 5774://iron			
+			case 5777://tin
+			case 5780://copper
+			case 5785://mithril
 			    return 5764;	
 			
 			case 5772://coal
 			case 5775://iron
+			case 5778://tin
 			case 5781://copper
 			case 5783://adamantite
+			case 5786://mithril
 			    return 5765;
-			
-			case 72075://clay
-			    return 72078;
-			
-			case 72077://clay
-			    return 72080;
-			
-			case 72081://iron
-			    return 72084;
-			
-			case 72082://iron
-			    return 72085;
-			
-			case 72083://iron
-			    return 72086;
-			
-            case 72087://gold
-			    return 72089;
-				
-			case 72088://gold
-			    return 72090;	
-				
-			case 72092://tin
-			    return 72095;	
-			
-			case 72094://tin
-			    return 72097;
-			
-			case 72098://copper
-			    return 72101;
-			
-			case 72099://copper
-			    return 72102;
-			
-			case 72100://copper
-			    return 72103;
-			
-			case 93016://mithril	
-			case 93018://coal
-			case 93020://adamantite
-			case 93022://runite
-			case 93024://gem rock
-			case 94218://soft clay
-				return 93014;
-			
-			case 93017://mithril
-			case 93019://coal
-			case 93021://adamantite
-			case 93023://runite
-			case 93025://gem rock
-			case 94219://soft clay
-				return 93015;
 
 			case 10578://clay	
 				return 11553;
@@ -402,14 +366,17 @@ module.exports = (function () {
 				
 			case 11930://coal
 			case 11933://tin
+			case 11936://copper
 				return 11552;
 				
 			case 11931://coal	
 			case 11934://tin
+			case 11943://mithril
 				return 11553;
 				
 			case 11932://coal
 			case 11935://tin
+			case 11938://copper
 				return 11554;	
 	
 			case 11948://silver	
@@ -452,6 +419,18 @@ module.exports = (function () {
 			case 14860://runite
 			case 14863://adamantite
 		        return 14833;	
+				
+			case 14902://tin	
+			case 14904://clay
+			case 14906://copper	
+			case 14913://iron
+				return 14892;
+				
+			case 14903://tin	
+			case 14905://clay
+			case 14907://copper	
+			case 14914://iron
+				return 14893;	
 			
 			case 21275://adamantite
 			case 21278://mithril
@@ -473,6 +452,92 @@ module.exports = (function () {
 			case 21286://copper
 			case 21289://coal
 				return 21298;
+			
+			case 29215://coal
+				return 29218;
+			
+			case 29216://coal
+				return 29219;
+				
+			case 29217://coal
+				return 29220;	
+			
+			case 67006://clay
+            case 67009://tin
+			case 67012://copper
+			    return 72802;
+			
+			case 67007://clay
+			case 67010://tin
+			case 67013://copper
+			    return 72803;
+				
+			case 67008://clay
+			case 67011://tin
+			case 67014://copper
+			    return 72804;	
+				
+			case 72075://clay
+			    return 72078;
+			
+			case 72076://clay
+			    return 72079;
+			
+			case 72077://clay
+			    return 72080;
+						
+			case 72081://iron
+			    return 72084;
+			
+			case 72082://iron
+			    return 72085;
+			
+			case 72083://iron
+			    return 72086;
+			
+            case 72087://gold
+			    return 72089;
+				
+			case 72088://gold
+			    return 72090;	
+				
+			case 72092://tin
+			    return 72095;	
+				
+			case 72093://tin
+			    return 72096;	
+			
+			case 72094://tin
+			    return 72097;
+			
+			case 72098://copper
+			    return 72101;
+			
+			case 72099://copper
+			    return 72102;
+			
+			case 72100://copper
+			    return 72103;	
+								
+			case 93016://mithril	
+			case 93018://coal
+			case 93020://adamantite
+			case 93022://runite
+			case 93024://gem rock
+				return 93014;
+			
+			case 93017://mithril
+			case 93019://coal
+			case 93021://adamantite
+			case 93023://runite
+			case 93025://gem rock
+				return 93015;	
+				
+			case 94218://soft clay
+				return 94216;	
+				
+			case 94219://soft clay	
+				return 94217;
 				
 		    default:
 		        return 3227;
